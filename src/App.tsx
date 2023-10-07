0f5e19c55a959b42e949a50154c0dee594655203
Quoc <quoc.cao@spiderbox.design>
2023-10-07 19:09:47 +0700

    init

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div className="bg-blue-gray-200 min-h-screen">
+      <div className="container mx-auto">
+        <div className="grid grid-cols-12 gap-4">
+          <div className="col-span-8 border">col 8</div>
+          <div className="col-span-4 border">col 4</div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
